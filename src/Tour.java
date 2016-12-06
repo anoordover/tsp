bca9c0b9468a5ed85799d0b0d7dee642a5ffff65
Arno Noordover <anoordover@users.noreply.github.com>
2016-12-06 20:47:30 +0100

    Eerste stap van "Part II" begonnen

--- src/Tour.java
+++ src/Tour.java
@@ -26,22 +26,103 @@ public class Tour {
     }
 
     public void show() {
-
+        //Als je helemaal geen eerst punt hebt kan je niets afdrukken
+        if (first != null) {
+            //Bijhouden waar je bent bij het afdrukken
+            Node current = first;
+            do {
+                StdOut.println(current.p.toString());
+                //Naar het volgende punt gaan in de route
+                current = current.next;
+                //Als je weer bij de eerste bent uitgekomen moet je stoppen
+            } while (current != first);
+        }
     }
 
     public void draw() {
-
+        //Als je helemaal geen eerst punt hebt kan je niets afdrukken
+        if (first != null) {
+            //Bijhouden waar je bent bij het afdrukken
+            Node current = first;
+            do {
+                current.p.drawTo(current.next.p);
+                //Naar het volgende punt gaan in de route
+                current = current.next;
+                //Als je weer bij de eerste bent uitgekomen moet je stoppen
+            } while (current != first);
+        }
     }
 
     public int size() {
-        return 1;
+        //Als je helemaal geen eerst punt hebt is de "size" 0
+        if (first == null) {
+            return 0;
+        }
+        int i = 0;
+        //Zie voor de rest commentaar bij "show"
+        Node current = first;
+        do {
+            i++;
+            current = current.next;
+        } while (current != first);
+        return i;
     }
 
     public double distance() {
-        return 0.0;
+        double distance = 0.0;
+        if (first != null) {
+            Node current = first;
+            do {
+                distance += current.p.distanceTo(current.next.p);
+                current = current.next;
+            } while (current != first);
+        }
+        return distance;
     }
 
     public void insertNearest(Point p) {}
 
     public void insertSmallest(Point p) {}
+
+    // main method for testing
+    public static void main(String[] args) {
+        // define 4 points forming a square
+        Point a = new Point(100.0, 100.0);
+        Point b = new Point(500.0, 100.0);
+        Point c = new Point(500.0, 500.0);
+        Point d = new Point(100.0, 500.0);
+
+        // Set up a Tour with those four points
+        // The constructor should link a->b->c->d->a
+        Tour squareTour = new Tour(a, b, c, d);
+
+        // Output the Tour
+        squareTour.show();
+        StdOut.println(squareTour.distance());
+
+        StdDraw.setXscale(0, 600);
+        StdDraw.setYscale(0, 600);
+        squareTour.draw();
+    }
+
+    public void insertInOrder(Point p) {
+        //TODO:
+        /*
+        Sam, hier moet je de opdracht van de eerst bullet uitvoeren.
+        Het punt p toevoegen aan het einde van de "linked list".
+        Let hierbij op het volgende:
+        Bij het toevoegen van het eerste punt is first gelijk aan null.
+        Hier moet je iets anders doen dan bij het toevoegen van de andere punten.
+         */
+        if (first == null) {
+            // hier moet je het aanmaken van de eerste Node doen en toekennen aan first.
+            // De next van deze node moet verwijzen naar first (een cirkel met 1 punt)
+        } else {
+            // Hier eerst het laatste punt in de linkedlist vinden.
+            // Vervolgens een nieuwe Node maken voor het nieuwe punt.
+            // Daarna "next" uit het laatste punt naar deze nieuwe node laten verwijzen
+            // Daarna "next" van jouw nieuwe node laten verwijzen naar first om
+            // de cyclus weer dicht te maken
+        }
+    }
 }
